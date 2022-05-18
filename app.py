c468b533a7bdce0c0b6954ff1ca1d4ad5dc6936f
Aaron Trinh <trinhaa@flip2.engr.oregonstate.edu>
2022-05-18 09:19:49 -0700

    Fixed nav bar routing.

--- app.py
+++ app.py
@@ -6,9 +6,9 @@ import os
 app = Flask(__name__)
 
 app.config['MYSQL_HOST'] = 'classmysql.engr.oregonstate.edu'
-app.config['MYSQL_USER'] = 'cs340_tolsone'
-app.config['MYSQL_PASSWORD'] = '3269' #last 4 of onid
-app.config['MYSQL_DB'] = 'cs340_tolsone'
+app.config['MYSQL_USER'] = 'cs340_trinhaa'
+app.config['MYSQL_PASSWORD'] = '5478' #last 4 of onid
+app.config['MYSQL_DB'] = 'cs340_trinhaa'
 app.config['MYSQL_CURSORCLASS'] = "DictCursor"
 
 
